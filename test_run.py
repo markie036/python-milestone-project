1cd2bb44fa57d18402e6177a644439021c0c84ea
Mark Harrison <mharrisondesign@mail.com>
2018-04-15 13:23:05 +0000

    Addition of test suite. Tests failing to start

---
+++ test_run.py
@@ -0,0 +1,21 @@
+import unittest
+from run import *
+
+def test_correct_answer():
+    return True
+    
+def test_wrong_answer():
+    return False
+
+"""Assert tests"""
+
+def test_correct_answer(): #function to test the question/answer on line 24
+    assert 4 + 4 == 9
+   
+def test_wrong_answer():
+    assert not 4 + 4 == 8 
+   
+test_correct_answer()
+test_wrong_answer()
+
+print("All the tests passed")
