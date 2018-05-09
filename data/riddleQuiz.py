d137e877e465da719ff38a25cf6f8a00bae88d2c
Mark Harrison <mharrisondesign@mail.com>
2018-05-09 19:50:28 +0000

    All functions for quiz

--- data/riddleQuiz.py
+++ data/riddleQuiz.py
@@ -1,6 +1,5 @@
 import json
 
-"""Code to run the quiz STARTS"""
 
 score = 0
 max_guesses = 3
@@ -35,6 +34,15 @@ def wrong_answer(answer, riddle):
 def no_of_guesses(answer, counter):
     if answer != riddle["answer"]:
         print("You have", max_guesses - counter, "guesses left")
+        
+def add_score(username, score):
+    # Add score to leaderboard
+    with open("data/leaderboard.txt", "a") as chat_list:
+        chat_list.writelines("({0}) {1} - {2}\n".format(
+            message_dict["timestamp"], 
+            message_dict["from"].title(), 
+            message_dict["message"]))
+    #messages.append("({}) {}: {}".format(now, username, message)) #brackets act as placeholders for time, user and message
 
 
 with open("riddles.json") as riddles_file:
@@ -42,5 +50,4 @@ with open("riddles.json") as riddles_file:
 
 for riddle in riddles: #loops through the quiz questions
     print(quiz(riddle))
- 
-"""Code to run the quiz ENDS"""   
+
